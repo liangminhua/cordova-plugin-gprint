da00daf6e43040e569612748f570fe8aa610033f
梁敏华 <ben302010@live.cn>
2016-07-13 23:39:32 +0800

    continue to fix

--- www/Gprinter.js
+++ www/Gprinter.js
@@ -1,23 +1,20 @@
 var exec = require('cordova/exec');
 
-exports.initService = function(arg0, success, error) {
+exports.initService = function (arg0, success, error) {
     exec(success, error, "Gprinter", "initService", [arg0]);
 };
-// exports.printTestPage = function(arg0, success, error) {
-//     exec(success, error, "Gprinter", "printTestPage", [arg0]);
-// };
-exports.openPort=function (arg0,success,error) {
-    exec(success,error,"Gprinter", "printTestPage",arg0)
+exports.openPort = function (arg0, success, error) {
+    exec(success, error, "Gprinter", "openPort", arg0);
 };
-exports.closePort=function (arg0,success,error) {
-    exec(success,error,"Gprinter", "printTestPage",arg0)
+exports.closePort = function (arg0, success, error) {
+    exec(success, error, "Gprinter", "closePort", arg0);
 };
-exports.sendEscCommand=function (arg0,success,error) {
-    exec(success,error,"Gprinter", "printTestPage",arg0)
+exports.sendEscCommand = function (arg0, success, error) {
+    exec(success, error, "Gprinter", "sendEscCommand", arg0);
 };
-exports.printTestPage=function (arg0,success,error) {
-    exec(success,error,"Gprinter", "printTestPage",arg0)
+exports.printTestPage = function (arg0, success, error) {
+    exec(success, error, "Gprinter", "printTestPage", arg0);
 };
-exports.resetEscCommand=function (arg0,success,error) {
-    exec(success,error,"Gprinter", "printTestPage",arg0)
+exports.resetEscCommand = function (arg0, success, error) {
+    exec(success, error, "Gprinter", "resetEscCommand", arg0);
 };
