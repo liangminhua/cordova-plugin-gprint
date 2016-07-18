6d95db1d0e4e0ab0131f6dde8ddf0cb579cd7b01
梁敏华 <ben302010@live.cn>
2016-07-18 16:09:43 +0800

    android almost done

--- src/android/Gprinter.java
+++ src/android/Gprinter.java
@@ -1,6 +1,5 @@
 package cordova.plugin.gprint;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -9,11 +8,9 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.util.Base64;
 
 import com.gprinter.aidl.GpService;
 import com.gprinter.command.EscCommand;
-import com.gprinter.command.GpCom;
 import com.gprinter.io.GpDevice;
 import com.gprinter.service.GpPrintService;
 
@@ -21,172 +18,282 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import java.util.Vector;
+import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Gprinter extends CordovaPlugin {
+
+    public static final String ACTION_CONNECT_STATUS = "action.connect.status";
+    private static final String PRINTER_ID = "printId";
+    private static final String PORT_TYPE = "portType";
+    private static final String DEVICE_NAME = "deviceName";
+    private static final String PORT_NUMBER = "portNumber";
+    private static final String TIME_OUT = "timeOut";
+    private static final String BASE64 = "base64";
+    private GpService gpService = null;
+    private PrinterServiceConnection printerServiceConnection = null;
+    private EscCommand escCommand= null;
+    private CallbackContext callbackContext;
+
     class PrinterServiceConnection implements ServiceConnection {
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            mGpService = null;
+            gpService = null;
         }
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            mGpService = GpService.Stub.asInterface(service);
+            gpService = GpService.Stub.asInterface(service);
         }
     }
 
-    public static final String ACTION_CONNECT_STATUS = "action.connect.status";
-    private Activity _this = cordova.getActivity();
-    private Context applicationContext = cordova.getActivity().getApplicationContext();
-    private GpService mGpService = null;
-    private PrinterServiceConnection printerServiceConnection = null;
-    private EscCommand escCommand = new EscCommand();
-
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
         if (action.equals("initService")) {
-            String message = args.getString(0);
-            this.initService(callbackContext);
+            initService();
             return true;
         }
-        if (action.equals("openPort")){
-            int PrinterId=args.getInt(0);
-            int PortType=args.getInt(1);
-            String DeviceName =args.getString(2);
-            int PortNumber= args.getInt(3);
-            this.openPort(PrinterId,PortType,DeviceName,PortNumber,callbackContext);
+        if (action.equals("openPort")) {
+            openPort(args);
             return true;
         }
-        if (action.equals("closePort")){
-            int PrinterId=args.getInt(0);
-            closePort(PrinterId,callbackContext);
+        if (action.equals("closePort")) {
+            closePort(args);
             return true;
         }
-        if (action.equals("sendEscCommand")){
-            int PrinterId=args.getInt(0);
-            String base64=args.getString(1);
-            this.sendEscCommand(PrinterId,base64,callbackContext);
+        if (action.equals("getPrinterConnectStatus")) {
+            getPrinterConnectStatus(args);
             return true;
         }
         if (action.equals("printTestPage")) {
-            int PrinterId=args.getInt(0);
-            printTestPage(PrinterId,callbackContext);
+            printTestPage(args);
             return true;
         }
-        if (action.equals("resetEscCommand")) {
-            resetEscCommand(callbackContext);
+        if (action.equals("queryPrinterStatus")) {
+            queryPrinterStatus(args);
+            return true;
+        }
+        if (action.equals("getPrinterCommandType")) {
+            getPrinterCommandType(args);
+            return true;
+        }
+        if (action.equals("sendEscCommand")) {
+            sendEscCommand(args);
+            return true;
+        }
+        if (action.equals("sendTscComma")) {
+            sendTscCommand(args);
+            return true;
+        }
+        if (action.equals("print")) {
+            test(args);
             return true;
         }
         return false;
     }
 
-    private void initService(final CallbackContext callbackContext) {
-        cordova.getThreadPool().execute(new Runnable() {
+    private void initService() {
+        Intent intent = new Intent(cordova.getActivity(), GpPrintService.class);
+        Context context = cordova.getActivity().getApplicationContext();
+        try {
+            context.startService(intent);
+            printerServiceConnection = new PrinterServiceConnection();
+            Thread.sleep(1000);
+            context.bindService(intent, printerServiceConnection, Context.BIND_AUTO_CREATE);
+            callbackContext.success();
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void openPort(final JSONArray args) {
+        checkInitService();
+        JSONObject jsonObject = getArgsObject(args);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_CONNECT_STATUS);
+        cordova.getActivity().registerReceiver(new BroadcastReceiver() {
             @Override
-            public void run() {
-                Intent intent = new Intent(_this, GpPrintService.class);
-                try {
-                    applicationContext.startService(intent);
-                    printerServiceConnection = new PrinterServiceConnection();
-                    Thread.sleep(1000);
-                    applicationContext.bindService(intent, printerServiceConnection, Context.BIND_AUTO_CREATE);
-                    callbackContext.success();
-                } catch (Exception e) {
-                    callbackContext.error(e.getMessage());
+            public void onReceive(Context context, Intent intent) {
+                if (ACTION_CONNECT_STATUS.equals(intent.getAction())) {
+                    int type = intent.getIntExtra(GpPrintService.CONNECT_STATUS, 0);
+                    int id = intent.getIntExtra(GpPrintService.PRINTER_ID, 0);
+                    if (type == GpDevice.STATE_CONNECTING) {
+                        callbackContext.success();
+                        cordova.getActivity().unregisterReceiver(this);
+                    } else if (type == GpDevice.STATE_NONE) {
+
+                    } else if (type == GpDevice.STATE_VALID_PRINTER) {
+
+                    } else if (type == GpDevice.STATE_INVALID_PRINTER) {
+
+                    }
                 }
             }
-        });
-    }
-    
-    private void openPort(int PrinterId, int PortType, String DeviceName, int PortNumber, final CallbackContext callbackContext) {
-        if (mGpService == null) {
-            callbackContext.error("No Init Service");
-            return;
-        }
+        }, intentFilter);
         try {
-            IntentFilter intentFilter = new IntentFilter();
-            intentFilter.addAction(ACTION_CONNECT_STATUS);
-            _this.registerReceiver(new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    if (ACTION_CONNECT_STATUS.equals(intent.getAction())) {
-                        int type = intent.getIntExtra(GpPrintService.CONNECT_STATUS, 0);
-                        int id = intent.getIntExtra(GpPrintService.PRINTER_ID, 0);
-                        if (type == GpDevice.STATE_CONNECTING) {
-                            callbackContext.success();
-                        } else if (type == GpDevice.STATE_NONE) {
-
-                        } else if (type == GpDevice.STATE_VALID_PRINTER) {
-
-                        } else if (type == GpDevice.STATE_INVALID_PRINTER) {
-
-                        }
-                    }
-
-                }
-            }, intentFilter);
-            mGpService.openPort(PrinterId, PortType, DeviceName, PortNumber);
-
+            int printId = jsonObject.getInt(PRINTER_ID);
+            int portType = jsonObject.getInt(PORT_TYPE);
+            String deviceName = jsonObject.getString(DEVICE_NAME);
+            int portNumber = jsonObject.getInt(PORT_NUMBER);
+            gpService.openPort(printId, portType, deviceName, portNumber);
+        } catch (JSONException e) {
+            callbackContext.error("Invaild Parameters");
         } catch (RemoteException e) {
-            callbackContext.error(e.getMessage());
+            callbackContext.error("Fail open port");
+        }
+
+    }
+
+    private void closePort(final JSONArray args) {
+        checkInitService();
+        JSONObject jsonObject = getArgsObject(args);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_CONNECT_STATUS);
+        cordova.getActivity().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (ACTION_CONNECT_STATUS.equals(intent.getAction())) {
+                    int type = intent.getIntExtra(GpPrintService.CONNECT_STATUS, 0);
+                    int id = intent.getIntExtra(GpPrintService.PRINTER_ID, 0);
+                    if (type == GpDevice.STATE_CONNECTING) {
+                        // callbackContext.success();
+                        cordova.getActivity().unregisterReceiver(this);
+                    } else if (type == GpDevice.STATE_NONE) {
+                        callbackContext.success();
+                        cordova.getActivity().unregisterReceiver(this);
+
+                    } else if (type == GpDevice.STATE_VALID_PRINTER) {
+
+                    } else if (type == GpDevice.STATE_INVALID_PRINTER) {
+
+                    }
+                }
+            }
+        }, intentFilter);
+        try {
+            int printId = jsonObject.getInt(PRINTER_ID);
+            gpService.closePort(printId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
 
-    private void closePort(int PrinterId, final CallbackContext callbackContext) {
-        if (mGpService == null) {
-            callbackContext.error("No Init Service");
-            return;
-        }
+    private void getPrinterConnectStatus(final JSONArray args) {
+        JSONObject jsonObject = getArgsObject(args);
         try {
-            mGpService.closePort(PrinterId);
+            int printId = jsonObject.getInt(PRINTER_ID);
+            //int timeOut = jsonObject.getInt(TIME_OUT);
+            int result = gpService.getPrinterConnectStatus(printId);
+            callbackContext.success(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
         } catch (RemoteException e) {
-            callbackContext.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private void printTestPage(int PrinterId, CallbackContext callbackContext) {
+    private void printTestPage(final JSONArray args) {
+        checkInitService();
+        JSONObject jsonObject = getArgsObject(args);
         try {
-            mGpService.printeTestPage(PrinterId);
-            callbackContext.success();
+            jsonObject.getInt(PRINTER_ID);
+            gpService.printeTestPage(0);
+        } catch (JSONException e) {
+            callbackContext.error("Invaild Parameters");
         } catch (RemoteException e) {
-            callbackContext.error(e.getMessage());
+            callbackContext.error("Fail Print Test Page");
         }
     }
 
-    private void sendEscCommand(int PrinterId, String base64, CallbackContext callbackContext) {
-        String sendString = null;
-        if (base64 == null) {
-            Vector<Byte> commands = escCommand.getCommand();
-            byte[] formatbytes = new byte[commands.size()];
-            for (int i = 0; i < commands.size(); i++) {
-                formatbytes[i] = commands.get(i);
+    private void queryPrinterStatus(final JSONArray args) {
+        JSONObject jsonObject = getArgsObject(args);
+        try {
+            int printId = jsonObject.getInt(PRINTER_ID);
+            int timeOut = jsonObject.getInt(TIME_OUT);
+            int result = gpService.queryPrinterStatus(printId,timeOut);
+            callbackContext.success(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getPrinterCommandType(final JSONArray args) {
+        JSONObject jsonObject = getArgsObject(args);
+        try {
+            int printId = jsonObject.getInt(PRINTER_ID);
+            int result = gpService.getPrinterCommandType(printId);
+            callbackContext.success(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendEscCommand(final JSONArray args) {
+        JSONObject jsonObject = getArgsObject(args);
+        try {
+            int printId = jsonObject.getInt(PRINTER_ID);
+            String base64 =jsonObject.getString(BASE64);
+            int result = gpService.sendEscCommand(printId,base64);
+            callbackContext.success(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendTscCommand(final JSONArray args) {
+        JSONObject jsonObject = getArgsObject(args);
+        try {
+            int printId = jsonObject.getInt(PRINTER_ID);
+            String base64 =jsonObject.getString(BASE64);
+            int result = gpService.sendLabelCommand(printId,base64);
+            callbackContext.success(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void test(final JSONArray args){
+
+    }
+
+    private void checkInitService() {
+        if (gpService == null)
+            callbackContext.error("please initService first!");
+    }
+
+    private JSONObject getArgsObject(JSONArray args) {
+        if (args.length() == 1) {
+            try {
+                return args.getJSONObject(0);
+            } catch (JSONException ex) {
             }
-            sendString = Base64.encodeToString(formatbytes, Base64.DEFAULT);
-        } else {
-            sendString = base64;
         }
+
+        return null;
+    }
+
+    private void addProperty(JSONObject obj, String key, Object value) {
+        //Believe exception only occurs when adding duplicate keys, so just ignore it
         try {
-            int ret = mGpService.sendEscCommand(PrinterId, sendString);
-            if (GpCom.ERROR_CODE.SUCCESS == GpCom.ERROR_CODE.values()[ret]) {
-                callbackContext.success();
+            if (value == null) {
+                obj.put(key, JSONObject.NULL);
             } else {
-                callbackContext.error("Send EscCommand Fail");
+                obj.put(key, value);
             }
-        } catch (RemoteException e) {
-            callbackContext.error(e.getMessage());
+        } catch (JSONException e) {
         }
-
     }
-
-    private void resetEscCommand(CallbackContext callbackContext){
-        escCommand = new EscCommand();
-        callbackContext.success();
-    }
-
 }
--- www/Gprinter.js
+++ www/Gprinter.js
@@ -1,20 +1,52 @@
-var exec = require('cordova/exec');
-
-exports.initService = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "initService", [arg0]);
-};
-exports.openPort = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "openPort", arg0);
-};
-exports.closePort = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "closePort", arg0);
-};
-exports.sendEscCommand = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "sendEscCommand", arg0);
-};
-exports.printTestPage = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "printTestPage", arg0);
-};
-exports.resetEscCommand = function (arg0, success, error) {
-    exec(success, error, "Gprinter", "resetEscCommand", arg0);
-};
+cordova.define("cordova-plugin-gprint.Gprinter", function (require, exports, module) {
+    var exec = require('cordova/exec');
+    var gprinterName = "Gprinter";
+    var bluetoothleName = "BluetoothLePlugin";
+    var gprinter = {
+        initService: function (success, error) {
+            alert("initService4");
+            cordova.exec(success, success, gprinterName, "initService", []);
+        },
+        stopService: function (success, error) {
+            alert("stopService");
+            cordova.exec(success, success, gprinterName, "stopService", []);
+        },
+        openPort: function (success, error, params) {
+            alert("openPort");
+            cordova.exec(success, success, gprinterName, "openPort", [params]);
+        },
+        closePort: function (success, error, params) {
+            alert("closePort");
+            cordova.exec(success, success, gprinterName, "closePort", [params]);
+        },
+        getPrinterConnectStatus: function (success, error, params) {
+            alert("getPrinterConnectStatus");
+            cordova.exec(success, error, gprinterName, "getPrinterConnectStatus", [params]);
+        },
+        printTestPage: function (success, error, params) {
+            alert("printTestPage");
+            cordova.exec(success, error, gprinterName, "printTestPage", [params]);
+        },
+        queryPrinterStatus: function (success, error, params) {
+            alert("queryPrinterStatus");
+            cordova.exec(success, error, gprinterName, "queryPrinterStatus", [params]);
+        },
+        getPrinterCommandType: function (success, error, params) {
+            alert("getPrinterCommandType");
+            cordova.exec(success, error, gprinterName, "getPrinterCommandType", [params]);
+        },
+        sendEscCommand: function (success, error, params) {
+            alert("sendEscCommand");
+            cordova.exec(success, error, gprinterName, "sendEscCommand", [params]);
+        },
+        sendTscCommand: function (success, error, params) {
+            alert("sendTscCommand");
+            cordova.exec(success, error, gprinterName, "sendTscCommand", [params]);
+        },
+        print:function(success,error,params){
+        alert("test");
+        cordova.exec(success,error,gprinterName,"test",[params]);
+        }
+    }
+    module.exports = gprinter;
+});
